1c5c1e6db49ce30c1a02b237e68151fa451e5b7a
unknown <kyomuhendoisihaka1@gmail.com>
2023-05-14 13:28:11 +0300

    updated

--- script.js
+++ script.js
@@ -97,3 +97,118 @@ inputs.forEach(input => {
 	input.addEventListener("blur", remcl);
 });
 
+// ---------- CHARTS ----------
+
+// BAR CHART
+var barChartOptions = {
+	series: [{
+	  data: [10, 8, 6, 4, 2],
+	  name: "Products",
+	}],
+	chart: {
+	  type: "bar",
+	  background: "transparent",
+	  height: 350,
+	  toolbar: {
+		show: false,
+	  },
+	},
+	colors: [
+	  "#2962ff",
+	  "#d50000",
+	  "#2e7d32",
+	  "#ff6d00",
+	  "#583cb3",
+	],
+	plotOptions: {
+	  bar: {
+		distributed: true,
+		borderRadius: 4,
+		horizontal: false,
+		columnWidth: "40%",
+	  }
+	},
+	dataLabels: {
+	  enabled: false,
+	},
+	fill: {
+	  opacity: 1,
+	},
+	grid: {
+	  borderColor: "#55596e",
+	  yaxis: {
+		lines: {
+		  show: true,
+		},
+	  },
+	  xaxis: {
+		lines: {
+		  show: true,
+		},
+	  },
+	},
+	legend: {
+	  labels: {
+		colors: "#f5f7ff",
+	  },
+	  show: true,
+	  position: "top",
+	},
+	stroke: {
+	  colors: ["transparent"],
+	  show: true,
+	  width: 2
+	},
+	tooltip: {
+	  shared: true,
+	  intersect: false,
+	  theme: "dark",
+	},
+	xaxis: {
+	  categories: ["Laptop", "Phone", "Monitor", "Headphones", "Camera"],
+	  title: {
+		style: {
+		  color: "#f5f7ff",
+		},
+	  },
+	  axisBorder: {
+		show: true,
+		color: "#55596e",
+	  },
+	  axisTicks: {
+		show: true,
+		color: "#55596e",
+	  },
+	  labels: {
+		style: {
+		  colors: "#f5f7ff",
+		},
+	  },
+	},
+	yaxis: {
+	  title: {
+		text: "Count",
+		style: {
+		  color:  "#f5f7ff",
+		},
+	  },
+	  axisBorder: {
+		color: "#55596e",
+		show: true,
+	  },
+	  axisTicks: {
+		color: "#55596e",
+		show: true,
+	  },
+	  labels: {
+		style: {
+		  colors: "#f5f7ff",
+		},
+	  },
+	}
+  };
+  
+  var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
+  barChart.render();
+  
+
